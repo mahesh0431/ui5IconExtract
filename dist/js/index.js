4a40ac5526463b9623f9ea7dd0f595be71e2203b
Mahesh <p.maheshkumarreddy@gmail.com>
2020-08-29 16:20:17 +0400

    new changes

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,15 +1,13 @@
-import {getUI5IconSVG, getUI5IconsDetails} from '../../src/UI5IconExtract';
+import {getUI5IconSVG, getIconsNames} from '../../src/UI5IconExtract';
 
 // readOpenUI5Icons().then(function(svg){
 //     var container = document.getElementById("svg-view");
 //     container.innerHTML = svg;
 //     console.log("Triggeredyola");
 // });
-getUI5IconsDetails().then(function(icons){
-    console.log(icons);
-    getUI5IconSVG("e035").then(function(svg){
-    var container = document.getElementById("svg-view");
-    container.innerHTML = svg;
-    console.log("Triggeredyola");
+console.log(getIconsNames());
+
+    getUI5IconSVG("add-employee").then(function(svg){
+        var container = document.getElementById("svg-view");
+            container.innerHTML = svg;
     });
-});
